3157407727035883223a535fe90de533821af3c1
hh9851 <ella980511@gmail.com>
2025-04-25 11:40:58 +0800

    ground

---
+++ ground/data.js
@@ -0,0 +1,297 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.9497825768638997,
+        "pitch": -0.01952914425164387,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.776129740715069,
+          "pitch": -0.0043923313928679875,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.20881262542687828,
+          "pitch": 0.033944936024141725,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8880685661328673,
+          "pitch": 0.5124553299981134,
+          "rotation": 9.42477796076938,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8965171004058394,
+          "pitch": -0.29884467163082107,
+          "title": "自由廣場",
+          "text": "2007年12月7日陳水扁政府落實去蔣化政策，將中正紀念堂廣場的「大中至正」牌樓易名並替換上「自由廣場」字樣，自由廣場是中華民國總統迎接外國政要來訪時的紅毯軍禮儀式以及許多大型活動租用的舉辦場地，曾舉辦過1990年至2000年的臺灣燈會、2000年至2004年及2006年與2007年的臺北燈節，也是臺灣社會運動1990年野百合學運與2008年野草莓運動的集會地點。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "國家戲劇院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.852994309297749,
+          "pitch": 0.28268489542085007,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.8163304511902894,
+          "pitch": 0.2897969249519168,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5723689613335647,
+          "pitch": -0.1148718661190351,
+          "title": "兩廳院上平台",
+          "text": "為兩廳院上之平台，常見學生等藝術團體在此地團練"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "兩廳院內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.12956008777458,
+        "pitch": 0.13916954849470997,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0924051550867695,
+          "pitch": 0.25512899088131213,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4480627094503227,
+          "pitch": 0.18441806977748243,
+          "title": "兩廳院內部",
+          "text": "紅色是兩廳院空間中隨處可見的代表色，包含建築、舞台布幕、座椅、地毯等。在與廣場的接口處常見許多自動販賣機以供解渴"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "兩廳院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.9249524392171651,
+        "pitch": -0.12721305039278263,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4121015268367145,
+          "pitch": 0.07980042104614782,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.63812285773405,
+          "pitch": -0.4790133494601534,
+          "title": "兩廳院",
+          "text": "國家兩廳院於1987年落成，開啟臺灣表演藝術劇場邁入專業化的先聲。兩廳院是亞洲最自由開放的藝術中心，也是國際具指標性的專業劇場，無論當代或傳統、原生或外來，都在兩廳院的舞臺上共生共存。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "中正紀念堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.400713348672177,
+        "pitch": -0.3129468481019373,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.39280782537918135,
+          "pitch": 0.15286455078168615,
+          "rotation": 9.42477796076938,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.20740683823214567,
+          "pitch": 0.04925916002201447,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0985196646622732,
+          "pitch": 0.41889376848107673,
+          "title": "中正紀念堂",
+          "text": "在整點時於階梯前有衛兵交接儀式"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "中正紀堂內部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5044383051324068,
+          "pitch": 0.19055957890054565,
+          "rotation": 3.9269908169872414,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4241073481215505,
+          "pitch": -0.18512509186987103,
+          "title": "蔣公銅像",
+          "text": "蔣中正總統銅像高6.3公尺，厚至6至12毫米，全重約21.25公噸，由陳一帆先生承鑄。臺座高3.5公尺，全高9.8公尺。"
+        }
+      ]
+    }
+  ],
+  "name": "廣場",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
